8db2b482152ddd72a0d7a7642cf8d835fc026adf
chitettimanasa-2008 <chittetimanasa22@gmail.com>
2026-09-10 13:02:21 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,32 @@
+
+#identity operators
+a = None
+print(a is None)
+print(a is not None)
+
+#bitwise operators
+a = 5
+b = 3
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+#electric city bill calculater
+units = int(input("Enter Electricty units"))
+
+rate = 6
+
+bill = units * rate
+
+print("Electricity Bill:",bill)
+
+#travel expense calculater
+travel = float(input("Travel expense:"))
+food = float(input("Food expense:"))
+hotel = float(input("Hotel expense:"))
+
+total = travel + food + hotel
+print("Total Expense:", total)
+
+#list in python
+
